d504d5d469ba4cdad54c081e605e1363abcf2546
Jufiel Lariosa <lariosajupilot1@gmail.com>
2026-07-11 03:00:59 +0800

    feat: update app version to 2.0.7 and upgrade dependencies in pubspec.lock

--- kiosk/lib/features/cashier_report/view/cashier_report_preview_screen.dart
+++ kiosk/lib/features/cashier_report/view/cashier_report_preview_screen.dart
@@ -7,6 +7,7 @@ import 'package:intl/intl.dart';
 import '../../../styles/color_set.dart';
 import '../../../styles/responsive/responsive_value.dart';
 import '../../../theme/pos_design.dart';
+import '../../../widgets/message_dialog.dart';
 import '../../../widgets/network_error_dialog.dart';
 import '../../../widgets/top_app_bar.dart';
 import '../../../widgets/windows_scaffold.dart';
@@ -207,6 +208,14 @@ class _PrintButton extends ConsumerWidget {
       if (next case MutationError(:final error)) {
         showNetworkErrorDialog(context, error: error);
       }
+      if (next is MutationSuccess) {
+        showMessageDialog(
+          context,
+          type: DialogType.success,
+          title: 'Report Printed',
+          message: 'The X-Reading report has been sent to the printer.',
+        );
+      }
     });
 
     return SizedBox(
@@ -255,6 +264,14 @@ class _ReprintButton extends ConsumerWidget {
     ref.listen(printAction, (prev, next) {
       if (next case MutationError(:final error)) {
         showNetworkErrorDialog(context, error: error);
+      }
+      if (next is MutationSuccess) {
+        showMessageDialog(
+          context,
+          type: DialogType.success,
+          title: 'Report Reprinted',
+          message: 'The X-Reading report has been sent to the printer.',
+        );
       }
     });
 
